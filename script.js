94f810fedd79cda3e9d774843743e1597d852e0f
adelina2999 <tataru.adelina99@gmail.com>
2019-07-09 22:00:18 +0300

    My first page

--- script.js
+++ script.js
@@ -1,4 +1,80 @@
-function myFunction() {
-    alert("Hello World!");
-  }
-  myFunction();
+/*function myFunction() {
+alert("Hello World!");
+}
+myFunction();*/ //Execute the function named myFunction.
+
+/*var person = {
+firstName: "John",
+lastName: "Doe"
+};
+alert(person.firstName);*/ //Alert "John" by extracting information from the person object.
+
+  /*var person = {
+  name : "John",
+  age : 50
+};
+alert (person.name + " is " + person.age);*/
+
+/*var txt = "Hello World!";
+var x = txt.length;
+alert(x);*/ //Use the length property to alert the length of txt.
+
+/*var txt = "We are \"Vikings\"";
+alert(txt);*/ //Use escape characters to alert We are "Vikings".
+
+/*var str1 = "Hello ";
+var str2 = "World!";
+alert(str1 + str2);*/ //Concatenate the two strings to alert "Hello World!".
+
+/*var txt = "abcdefghijklm";
+var pos = txt.indexOf("h");
+alert(pos);*/ //Find the position of the character h in the string txt.
+
+/*var txt = "I can eat bananas all day";
+var x = txt.slice(10, 17);
+alert(x);*/ //Use the slice method to return the word "bananas".
+
+/*var txt = "Hello World";
+txt = txt.replace("Hello", "Welcome");
+alert(txt);*/ //Use the correct String method to replace the word "Hello" with the word "Welcome".
+
+/*var txt = "Hello World";
+txt = txt.toUpperCase();
+alert(txt);*/ //Convert the value of txt to upper case.
+
+/*var txt = "Hello World";
+txt = txt.toLowerCase();
+alert(txt);*/ //Convert the value of txt to lower case.
+
+/*var str1 = "Hello ";
+var str2 = "World!";
+var result = str1.concat(str2);
+alert(result);*/ //Use the concat() method create a string that says "Hello World!".
+
+/*var cars = ["Saab", "Volvo", "BMW"];
+var x = cars[1];
+alert(x);*/  //Get the value "Volvo" from the cars array.
+
+/*var cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0] = "Ford";
+alert(cars[0]);*/ //Change the first item of cars to "Ford".
+
+/*var cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);*/ //Alert the number of items in an array, using the correct Array method.
+
+/*var fruits = ["Banana", "Orange", "Apple"];
+fruits.pop();
+alert(fruits);*/ //Use the correct Array method to remove the last item of the fruits array.
+
+/*var fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Kiwi");
+alert(fruits);*/ //Use the correct Array method to add "Kiwi" to the fruits array.
+
+/*var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.splice(1, 2);
+alert(fruits);*/ //Use the splice() method to remove "Orange" and "Apple" from fruits.
+
+/*var girls = ["Cecilie", "Lone"];
+var boys = ["Emil", "Tobias", "Linus"];
+var children = girls.concat(boys);
+alert(children);*/ //Use the concat() method to concatenate girls and boys.
